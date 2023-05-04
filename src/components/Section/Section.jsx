b536393d27d41b0d3b5f427ba61b944c2d684c11
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-05-04 09:50:01 +0500

    commit

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -10,24 +10,18 @@ import axios from "axios";
 function Section() {
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgzMTkzMjM5LCJpYXQiOjE2ODMxMDY4MzksImp0aSI6IjA4M2UyMzQyMWVkZDQ5MDRhZDE0YjYzY2ZhN2Y3MmY3IiwidXNlcl9pZCI6MjB9.wrcq4jb_-TP0kamtWjndxDEV15Ca_5kK3zKsEKoC8AU";
-  // const communityCreateData = JSON.parse(getItem("communityCreate"));
   const [selectedOption, setSelectedOption] = useState({ value: 1, label: 1 });
   const [list, setList] = useState([]);
-  // const [itemOffset, setItemOffset] = useState(0);
-  // const endOffset = itemOffset + selectedOption.value;
-  // const currentItems = list?.slice(itemOffset, endOffset);
-  // const pageCount = Math.ceil(list?.length / selectedOption.value);
-  // const handlePageClick = (event) => {
-  //   const newOffset = (event.selected * selectedOption.value) % list?.length;
-  //   console.log(
-  //     `User requested page number ${event.selected}, which is offset ${newOffset}`
-  //   );
-  //   setItemOffset(newOffset);
-  // };
+  const [itemOffset, setItemOffset] = useState(0);
+  const pageCount = Math.ceil(list?.count / selectedOption.value);
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * selectedOption.value) % list?.count;
+    setItemOffset(newOffset);
+  };
 
   useEffect(() => {
     axios
-      .get("https://backend.minalappar.se/files/", {
+      .get("https://backend.minalappar.se/files/?organ=s", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -36,14 +30,12 @@ function Section() {
       .catch((err) => console.log(err));
   }, []);
 
-  console.log(list);
-
   return (
     <div className="section">
       <div className="container">
         <div className="section-list">
           <label htmlFor="">
-            <img  src={SearchIcon} alt="" className="section-img" />
+            <img src={SearchIcon} alt="" className="section-img" />
             <input
               type="text"
               placeholder="Search..."
@@ -53,13 +45,14 @@ function Section() {
           </label>
           <button className="section-btn">SÖK</button>
         </div>
+        <p>{list.count}</p>
         <SimpleGrid>
           {list?.results?.map((evt) => (
             <SectionCard evt={evt} key={evt.id} />
           ))}
         </SimpleGrid>
 
-        {/* <ReactPaginate
+        <ReactPaginate
           breakLabel="..."
           nextLabel="next >"
           onPageChange={handlePageClick}
@@ -67,7 +60,17 @@ function Section() {
           pageCount={pageCount}
           previousLabel="< previous"
           renderOnZeroPageCount={null}
-        /> */}
+          subContainerClassName="pages"
+          breakLinkClassName="page_link"
+          containerClassName="pagination"
+          pageClassName="page_item"
+          pageLinkClassName="page_link"
+          previousClassName="page_item"
+          previousLinkClassName="page_link"
+          nextClassName="page_item"
+          nextLinkClassName="page_link"
+          activeClassName="active"
+        />
       </div>
     </div>
   );
